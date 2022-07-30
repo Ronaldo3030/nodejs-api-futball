a66860f09491933f8c578240c72ae77e52058c05
ronaldo3030 <ronaldomacielcamposjunior@gmail.com>
2022-07-30 11:11:58 -0300

    add novas rotas para jogos de ontem e de agora

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const server = express();
 const bot = require('./jogo');
+const botOntem = require('./jogo-ontem');
+const botAgora = require('./jogo-online');
 
 const port = process.env.PORT || 3000;
 
 server.get('/', async(req, res) => {
     const response = await bot();
+    res.send(response);
+})
+
+server.get('/ontem', async(req, res) => {
+    const response = await botOntem();
+    res.send(response);
+})
+server.get('/agora', async(req, res) => {
+    const response = await botAgora();
     res.send(response);
 })
 
---
+++ jogo-online.js
@@ -0,0 +1,38 @@
+const pup = require('puppeteer');
+
+const url = 'https://fscore.com.br/live';
+
+// TIMES COM NOME - TÃO BUGANDO
+
+const bot = async () => {
+    const list = [];
+    const browser = await pup.launch({
+        headless: true,
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    });
+    const page = await browser.newPage();
+    console.log("INICIADO!");
+
+    await page.goto(url);
+    console.log("Foi para url.");
+
+    await page.waitForSelector('.index');
+
+    const jogos = await page.$$eval('.match-grid-title', el => el.map(nome => nome.innerText));
+
+
+    jogos.forEach(jogo => {
+        const obj = {};
+        let times = jogo.split('-');
+        obj.time_casa = times[0];
+        obj.time_fora = times[1];
+
+        list.push(obj);
+    });
+
+    await browser.close();
+    console.log("Trago todos")
+    return list;
+};
+
+module.exports = bot;
